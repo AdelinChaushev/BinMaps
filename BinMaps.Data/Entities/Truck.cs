f465d26bca119048664e3a09e8559281c2aa2c7e
Adelin <adelinchaushev@gmail.com>
2025-11-29 04:13:35 +0200

    Added Database

---
+++ BinMaps.Data/Entities/Truck.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinMaps.Data.Entities
+{
+	public class Truck
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public double Capacity { get; set; }
+		[ForeignKey(nameof(Area))]
+		public int AreaId { get; set; }
+		public Area Area { get; set; }
+	}
+}
---
+++ BinMaps/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using BinMaps.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BinMaps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
