71cf93911f552f3a99df7df90cad59c343639a11
Adelin <adelinchaushev@gmail.com>
2025-11-30 08:25:15 +0200

    Adde get drivers to the admin.

--- BinMaps.Data/BinMapsDbContext.cs
+++ BinMaps.Data/BinMapsDbContext.cs
@@ -36,6 +36,74 @@ namespace BinMaps.Data
                 .HasOne(a => a.Truck)
                 .WithOne(t => t.Area)
                 .HasForeignKey<Truck>(t => t.AreaId);
+
+            modelBuilder.Entity<Area>().HasData(new
+            {
+                Id = 1,
+                Name = "South",
+                IsFull = false,
+                LitersFilled = 0.0
+
+            },new
+            {
+                Id = 2,
+                Name = "North",
+                IsFull = false,
+                LitersFilled = 0.0
+            });
+           // Relative to project root
+
+            // Example: Read and seed
+            modelBuilder.Entity<IdentityRole>().HasData(
+               new IdentityRole()
+               {
+                     Id = "1",
+                     Name = "Admin",
+                     NormalizedName = "ADMIN"
+               },
+                new IdentityRole()
+                {
+                     Id = "2",
+                     Name = "Driver",
+                     NormalizedName = "DRIVER"
+                }
+            );
+          
+            modelBuilder.Entity<ReportType>().HasData(
+                new ReportType()
+                {
+                    Id = 1,
+                    Name = "Damaged Container"
+                },
+                new ReportType()
+                {
+                    Id = 2,
+                    Name = "Overflowing Container"
+                },
+                new ReportType()
+                {
+                    Id = 3,
+                    Name = "Incorrect Location"
+                }
+            );
+            modelBuilder.Entity<Truck>().HasData(
+                new Truck()
+                {
+                    Id = 1,
+                    Capacity = 15000.0,
+                    AreaId = 1,
+                    DriverId = "e02c3145-1063-4de6-9ecb-f4cb0cea7667"
+                },
+                new Truck()
+                {
+                    Id = 2,
+                    Capacity = 15000.0,
+                    AreaId = 2,
+                    DriverId = "a8f43cdd-e1c1-4c7c-af01-3a4a8201a95b"
+                }
+            );
+
+
         }
 
     }
